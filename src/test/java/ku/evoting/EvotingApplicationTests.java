3fe068bbc5a38f59df770e293e46535113da029f
Ocean Chime <oceanchime@gmail.com>
2019-04-26 23:54:21 +0700

    add test

--- src/test/java/ku/evoting/EvotingApplicationTests.java
+++ src/test/java/ku/evoting/EvotingApplicationTests.java
@@ -2,15 +2,41 @@ package ku.evoting;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class EvotingApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private PartyRepository repository;
+
 	@Test
-	public void contextLoads() {
+	public void getFirstParty() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/party/1").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"พลังประชารัฐ\",\"voteCount\":8433137,\"electedMemberCount\":97,\"partylistCount\":0}")));
 	}
+
+//	@Test
+//	public void getHello() throws Exception {
+//		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+//				.andExpect(status().isOk())
+//				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+//	}
 
 }
